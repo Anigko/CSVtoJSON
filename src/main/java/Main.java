94dc5307687d829a2aa926e6e63571307ee7eb29
Anigko <anigko@yandex.ru>
2022-04-09 21:38:23 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,61 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> list = parseCSV();
+        String json = listToJson(list);
+        writeToJson(json);
+    }
+
+    private static List<Employee> parseCSV() {
+        List<Employee> list = null;
+        try (CSVReader reader = new CSVReader(new FileReader("data.csv"))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping("id", "firstName", "lastName", "country", "age");
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            list = csv.parse();
+            list.forEach(System.out::println);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
+    private static <T> String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<T>>() {
+        }.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String json = gson.toJson(list, listType);
+        System.out.println(json);
+        return json;
+    }
+
+    private static void writeToJson(String json) {
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
